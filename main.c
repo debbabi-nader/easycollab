e6c676c6e533bc455e81dba02dbf451c0cfd9ece
Mohamed Firas Zribi <zribimohamedfiras@gmail.com>
2017-12-06 22:21:04 +0100

    ajout add_project

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "./signup/signup.h"
 #include "./user-dashboard/user-dashboard.h"
 #include "./login/login.h"
+#include "./addproject/addproject.h"
 
 
 int main (int argc, char **argv)
@@ -17,6 +18,7 @@ int main (int argc, char **argv)
   //showUserDashboardWindow(NULL, NULL);
   //showSignupWindow(NULL, NULL);
   showLoginWindow(NULL, NULL);
+  //showAddProjectWindow(NULL, NULL);
   gtk_main ();
   return 0;
 
